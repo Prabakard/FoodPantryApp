73895f09fc4ce9a210f18cf84433df6753241313
Prabakar <prabakar.microsun@gmail.com>
2014-12-07 23:01:08 -0500

    added model classes

--- FoodPantryApp/app/src/main/java/in/microsun/foodpantryapp/Entities/Lookup.java
+++ FoodPantryApp/app/src/main/java/in/microsun/foodpantryapp/Entities/Lookup.java
@@ -1,7 +1,77 @@
 package in.microsun.foodpantryapp.Entities;
 
-/**
- * Created by Prabakard on 12/5/2014.
- */
+import java.util.HashMap;
+import java.util.Map;
+
+
+
 public class Lookup {
+
+    private int Id;
+    private String Name;
+    private String Type;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     *
+     * @return
+     * The Id
+     */
+    public int getId() {
+        return Id;
+    }
+
+    /**
+     *
+     * @param Id
+     * The Id
+     */
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    /**
+     *
+     * @return
+     * The Name
+     */
+    public String getName() {
+        return Name;
+    }
+
+    /**
+     *
+     * @param Name
+     * The Name
+     */
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    /**
+     *
+     * @return
+     * The Type
+     */
+    public String getType() {
+        return Type;
+    }
+
+    /**
+     *
+     * @param Type
+     * The Type
+     */
+    public void setType(String Type) {
+        this.Type = Type;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
 }
--- FoodPantryApp/app/src/main/java/in/microsun/foodpantryapp/Entities/MemberVisit.java
+++ FoodPantryApp/app/src/main/java/in/microsun/foodpantryapp/Entities/MemberVisit.java
@@ -1,7 +1,75 @@
 package in.microsun.foodpantryapp.Entities;
 
-/**
- * Created by Prabakard on 12/5/2014.
- */
+import java.util.HashMap;
+import java.util.Map;
+
 public class MemberVisit {
+
+    private int Id;
+    private Member Member;
+    private String VisitedOn;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     *
+     * @return
+     * The Id
+     */
+    public int getId() {
+        return Id;
+    }
+
+    /**
+     *
+     * @param Id
+     * The Id
+     */
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    /**
+     *
+     * @return
+     * The Member
+     */
+    public Member getMember() {
+        return Member;
+    }
+
+    /**
+     *
+     * @param Member
+     * The Member
+     */
+    public void setMember(Member Member) {
+        this.Member = Member;
+    }
+
+    /**
+     *
+     * @return
+     * The VisitedOn
+     */
+    public String getVisitedOn() {
+        return VisitedOn;
+    }
+
+    /**
+     *
+     * @param VisitedOn
+     * The VisitedOn
+     */
+    public void setVisitedOn(String VisitedOn) {
+        this.VisitedOn = VisitedOn;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
 }
